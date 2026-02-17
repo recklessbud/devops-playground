5a6ee9d667d1b958874bba5b73b7c7401ca1bfc6
alexander <alexanderglover478@gmail.com>
2026-02-17 23:59:17 +0000

    format fix for dns resolution script

--- Linux-troubleshot-lab/scenarios/network-dns/fix.sh
+++ Linux-troubleshot-lab/scenarios/network-dns/fix.sh
@@ -1,10 +1,14 @@
-# fix dns resolution
+## fix dns resolution
 
-# check the /etc/resolve.conf file
+    # check the /etc/resolve.conf file
 
 cat /etc/resolve.conf
 resolvectl status
 dig goole.com
 nslookup google.com
 
-# almost the same job for both
+    # almost the same job for both
+
+    # now restore the original /etc/resolve.conf file
+
+
